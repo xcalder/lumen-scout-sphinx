8ca5fbabb971899992f738faef511f78f5b8dcee
xcalder <xcalder@foxmail.com>
2019-01-16 18:36:50 +0800

    00

---
+++ src/Provider/SphinxServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sphinx\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use Sphinx\Engine\SphinxEngine;
+use Laravel\Scout\EngineManager;
+
+class SphinxServiceProvider extends ServiceProvider
+{
+    
+    public function boot()
+    {
+        resolve(EngineManager::class)->extend('sphinx', function ($app) {
+            return new SphinxEngine(config('scout.sphinx'));
+        });
+    }
+    
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(EngineManager::class, function ($app) {
+            return new EngineManager($app);
+        });
+    }
+}
